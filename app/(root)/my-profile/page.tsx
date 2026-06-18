10c70ed9247e60d603ebc6afc92da1c2e968e963
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-06-18 19:45:07 +0530

    my-profile fixed to load books from db

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { signOut } from "@/auth";
+import {signOut} from "@/auth";
 import BookList from "@/components/BookList";
-import { sampleBooks } from "@/constants";
+import {db} from "@/database/drizzle";
+import {books} from "@/database/schema";
+import {desc} from "drizzle-orm";
 
-const Page = () => {
+const Page = async () => {
+
+    const latestBooks = (await db
+        .select()
+        .from(books)
+        .limit(10)
+        .orderBy(desc(books.createdAt))) as Book[];
+
     return (
         <>
             <form
@@ -18,7 +27,7 @@ const Page = () => {
                 <Button>Logout</Button>
             </form>
 
-            <BookList title="Borrowed Books" books={sampleBooks} />
+            <BookList title="Borrowed Books" books={latestBooks} />
         </>
     );
 };
